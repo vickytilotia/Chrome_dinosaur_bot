417d2e9962a99b9bf33dcd0822abac242e91fb1b
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2021-01-22 22:14:28 +0530

    try test

--- dino.py
+++ dino.py
@@ -9,7 +9,7 @@ def hit(key):
     pyautogui.keyDown(key)
     return
 
-def isCollide(data):
+"""def isCollide(data):
     # Draw the rectangle for birds
     for i in range(300, 415):
         for j in range(410, 563):
@@ -23,7 +23,7 @@ def isCollide(data):
                 hit("up")
                 return
     return
-
+"""
 if __name__ == "__main__":
     print("Hey.. Dino game about to start in 3 seconds")
     time.sleep(2)
@@ -32,21 +32,23 @@ if __name__ == "__main__":
     while True:
         image = ImageGrab.grab().convert('L')  
         data = image.load()
-        isCollide(data)
+        image.show()
+        #isCollide(data)
             
         # print(asarray(image))
-        '''
+        
         # Draw the rectangle for cactus
-        for i in range(275, 325):
-            for j in range(563, 650):
+        for i in range(175, 200):
+            for j in range(380, 460):
                 data[i, j] = 0
         
         # Draw the rectangle for birds
-        for i in range(250, 300):
-            for j in range(410, 563):
+        for i in range(200, 230):
+                
+            for j in range(300, 360):
                 data[i, j] = 171
 
         image.show()
         break
-      '''
+      
 
